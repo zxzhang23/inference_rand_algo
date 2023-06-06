49b2cd44b96ae58e76e92922a8b176b036e5237f
Zhixiang Zhang <zzxiang0710@gmail.com>
2023-06-06 13:48:40 -0400

    Update Figure_HGDP.R

--- Experiment/HGDP/Figure_HGDP.R
+++ Experiment/HGDP/Figure_HGDP.R
@@ -3,26 +3,24 @@
 
 library(PropCIs)
 library(ggplot2)
-#############
-#load the results, grid_m, and the first coordinate of the real solution
+
+### load the results, grid_m, and the first coordinate of the real solution
 conf0= read.csv("C:/Users/zhixz/Dropbox (Personal)/R/sketching/code_github/0hgdp_conf.csv")
 conf=conf0[,-1]
 sim=nrow(conf)
 grid_m=seq(400,800,50)
 ls[1]<-0.03470465
 
-#######################
 
+### Coverage of 90\% intervals for the first coordinate of $\beta_n$,  and 95\% Clopper-Pearson interval for the coverage 
 even<-seq(2,ncol(conf),2);odd<-seq(1,ncol(conf)-1,2)
 right<-conf[,even];left<-conf[,odd]
 accept<-(left<ls[1])&(ls[1]<right)
 cov0<-apply(accept,2,sum)
-
 cov<-matrix(cov0,ncol(conf)/(2*length(grid_m)),length(grid_m),byrow=TRUE)
-ratio<-cov/sim
+ratio<-cov/sim  #  rows represent different methods, and columns represent different sketching size m
 
-ci<-function(x){exactci(x,sim,0.95)$conf.int[1:2]}
-
+ci<-function(x){exactci(x,sim,0.95)$conf.int[1:2]} 
 int=matrix(0,ncol(conf)/(2*length(grid_m)),2*length(grid_m))
 for(i in 1:(ncol(conf)/(2*length(grid_m)))){
   for(j in 1:length(grid_m)){
@@ -33,13 +31,13 @@ for(i in 1:(ncol(conf)/(2*length(grid_m)))){
 lower<-int[,seq(1,2*length(grid_m),2)]
 upper<-int[,seq(2,2*length(grid_m),2)]
 
-library(ggplot2)
+
 x <- grid_m
 df_full <- data.frame(x = grid_m, y1 =ratio[1,], y2=ratio[2,],y3=ratio[3,],y4=ratio[4,],y5=ratio[5,], y_upper1 =upper[1,] ,
                       y_lower1 = lower[1,],y_upper2=upper[2,],y_lower2=lower[2,],y_upper3=upper[3,],y_lower3=lower[3,],
                       y_upper4=upper[4,],y_lower4=lower[4,],y_upper5=upper[5,],y_lower5=lower[5,])
 
-
+### Length of confidence intervals 
 len<-apply(right-left,2,mean)
 conflen<-matrix(len,5,length(grid_m),byrow=TRUE)
 len_sd<-apply(right-left,2,sd)
@@ -49,6 +47,7 @@ upp<-conflen+lensd
 
 df_len<-data.frame(x=grid_m,y1=conflen[1,],y2=conflen[2,],y3=conflen[3,],y4=conflen[4,],y5=conflen[5,],
                    low1=low[1,],low2=low[2,],low4=low[4,],low5=low[5,],upp1=upp[1,],upp2=upp[2,],upp4=upp[4,],upp5=upp[5,])
+
 
 
 ggplot(df_full, aes(x = x)) +
