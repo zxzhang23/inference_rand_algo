95fc228d44cd137e496c3b50698a87b153f9b918
Zhixiang Zhang <zzxiang0710@gmail.com>
2023-06-05 20:31:17 -0400

    Update and rename HGDP_experiment to HGDP_experiment.R

--- HGDP_experiment.R
+++ HGDP_experiment.R
@@ -5,6 +5,7 @@ source("inference_methods_hadamard.R")
 hgdp <- read.csv("C:/Users/zhixz/Dropbox (Personal)/R/sketching/code_randomized_algorithms/real data analysis/hgdp.csv", header =TRUE, sep = ",")
 
 
+####  select the first 200 features to form X, and the next feature as y
 X0<-as.matrix(hgdp[,1:200])
 y0<-as.matrix(hgdp[,201])
 X<-scale(X0);y<-scale(y0)
@@ -12,9 +13,9 @@ n<-nrow(X);p<-ncol(X)
 ls<-qr.solve(X,y)
 c<-c(1,rep(0,p-1))
 print(sum(c*ls))
+
+
 grid_m=seq(400,800,50)
-
-
 res_hadamard<-compare_methods_hadamard(c,X,y,grid_m,300,K=100,sim=5,alpha=0.1)
 conf<-cbind(res_hadamard$pivo_conf,res_hadamard$sub_conf,res_hadamard$boot_conf,res_hadamard$plug_conf,res_hadamard$multi_run_conf)
 write.csv(conf,"0hgdp_conf.csv")
