1b2e54c67c1a34af2e867a747a04f00138d6bd1f
Zhixiang Zhang <zzxiang0710@gmail.com>
2023-06-08 15:31:12 -0400

    Update pivotal_inference.R

--- code/pivotal_inference.R
+++ code/pivotal_inference.R
@@ -1,56 +1,66 @@
 
 ### pivotal inference from sketching estimators of different types,
 ### including i.i.d., uniform orthogonal, and Hadamard
-pivo_iid<-function(c,n,sX,sy,ls_sk,partial=0,alpha){
+### for partial sketching, assume that $X^\top y$ is known
+pivo_iid<-function(c,n,sX,sy,partial=0,alpha){
   m<-nrow(sX);p<-ncol(sX)
   xi<-p/n;gamma<-m/n
-  inv_sX<-solve((t(sX)%*%sX))%*%t(sX)
-  H_XS<-sX%*%inv_sX
-  sep<-sy-H_XS%*%sy
+  invsX<-solve((t(sX)%*%sX))
   if(partial==0){
-    va<-sum((t(c)%*%inv_sX)^2)*sum((t(sy)%*%(diag(1,nrow(H_XS))-H_XS))^2)
-    est_v<-(gamma/((gamma-xi)))*va
+    lssk<-invsX%*%(t(sX)%*%sy)
+    center<-sum(c*lssk)
+    sep<-sy-sX%*%lssk
+    est_v<-(gamma/((gamma-xi)))*sum(c*(invsX%*%c))*sum(sep^2)
   }
   else{
-    est_v<-((gamma-xi)/gamma)*((sum((sX%*%ls_sk)^2)*sum((t(c)%*%inv_sX)^2)+(sum(c*ls_sk))^2))
+    lssk<-invsX%*%Xty
+    center<-(m-p)*sum(c*lssk)/m
+    est_v<-(gamma/(gamma-xi))*((sum((sX%*%lssk)^2)*sum(c*(invsX%*%c))+(sum(c*lssk))^2))
   }
   rb=qnorm(1-alpha/2,sd=sqrt(est_v/m))
   lb=qnorm(alpha/2,sd=sqrt(est_v/m))
-  list(conf=c(lb,rb))
+  
+  list(conf=c(center-rb,center-lb))
+}
+
+pivo_haar<-function(c,n,sX,sy,partial=0,alpha){
+  m<-nrow(sX);p<-ncol(sX)
+  xi<-p/n;gamma<-m/n
+  invsX<-solve((t(sX)%*%sX))
+  if(partial==0){
+    lssk<-invsX%*%(t(sX)%*%sy)
+    center<-sum(c*lssk)
+    sep<-sy-sX%*%lssk
+    est_v<-(gamma*(1-gamma)/((gamma-xi)*(1-xi)))*sum(c*(invsX%*%c))*sum(sep^2)
+  }
+  else{
+    lssk<-invsX%*%Xty
+    center<-((gamma-xi)/(gamma*(1-xi)))*sum(c*lssk)
+    est_v<-((1-gamma)*(gamma-xi)/(gamma*(1-xi)^3))*((sum((sX%*%lssk)^2)*sum(c*(invsX%*%c))+(sum(c*lssk))^2))
+  }
+  rb=qnorm(1-alpha/2,sd=sqrt(est_v/m))
+  lb=qnorm(alpha/2,sd=sqrt(est_v/m))
+  list(conf=c(center-rb,center-lb))
 }
 
 
-
-pivo_haar<-function(c,n,sX,sy,ls_sk,partial=0,alpha){
+pivo_hadamard<-function(c,n,sX,sy,partial=0,alpha){
   m<-nrow(sX);p<-ncol(sX)
   xi<-p/n;gamma<-m/n
-  inv_sX<-solve((t(sX)%*%sX))%*%t(sX)
-  H_XS<-sX%*%inv_sX
+  invsX<-solve((t(sX)%*%sX))
   if(partial==0){
-    va<-sum((t(c)%*%inv_sX)^2)*sum((t(sy)%*%(diag(1,nrow(H_XS))-H_XS))^2)
-    est_v<-(gamma*(1-gamma)/((gamma-xi)*(1-xi)))*va
+    lssk<-invsX%*%(t(sX)%*%sy)
+    center<-sum(c*lssk)
+    sep<-sy-sX%*%lssk
+    est_v<-(gamma*(1-gamma)/((gamma-xi)*(1-xi)))*sum(c*(invsX%*%c))*sum(sep^2)
   }
   else{
-    est_v<-((1-gamma)*(gamma-xi)/(gamma*(1-xi)^3))*((sum((sX%*%ls_sk)^2)*sum((t(c)%*%inv_sX)^2)+(sum(c*ls_sk))^2))
+    lssk<-invsX%*%Xty    ### for partial case, we assume the knowledge of $X^\top y$
+    center<-((gamma-xi)/(gamma*(1-xi)))*sum(c*lssk)
+    est_v<-((1-gamma)*(gamma-xi)/(gamma*(1-xi)^3))*((sum((sX%*%lssk)^2)*sum(c*(invsX%*%c))+2*(sum(c*lssk))^2))  
+    ### the asymptotic variance of Hadamard partial sketching estimators are slightly different from that of Haar case
   }
   rb=qnorm(1-alpha/2,sd=sqrt(est_v/m))
   lb=qnorm(alpha/2,sd=sqrt(est_v/m))
-  list(conf=c(lb,rb))
-}
-
-
-pivo_hadamard<-function(c,n,sX,sy,ls_sk,partial=0,alpha){
-  m<-nrow(sX);p<-ncol(sX)
-  xi<-p/n;gamma<-m/n
-  inv_sX<-solve((t(sX)%*%sX))%*%t(sX)
-  H_XS<-sX%*%inv_sX
-  if(partial==0){
-    est_v<-((gamma*(1-gamma)/((gamma-xi)*(1-xi)))*sum((t(c)%*%inv_sX)^2)*sum((t(sy)%*%(diag(1,nrow(H_XS))-H_XS))^2))
-  }
-  else{
-    est_v<-((1-gamma)*(gamma-xi)/(gamma*(1-xi)^3))*((sum((sX%*%ls_sk)^2)*sum((t(c)%*%inv_sX)^2)+2*(sum(c*ls_sk))^2))
-  }
-  rb=qnorm(1-alpha/2,sd=sqrt(est_v/m))
-  lb=qnorm(alpha/2,sd=sqrt(est_v/m))
-  list(conf=c(lb,rb))
+  list(conf=c(center-rb,center-lb))
 }
