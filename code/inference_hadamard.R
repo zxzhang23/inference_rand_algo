2367348b4eb04251d7dcb81abf985f7973c67597
Zhixiang Zhang <zzxiang0710@gmail.com>
2023-07-08 18:15:28 -0400

    Update inference_hadamard.R

--- code/inference_hadamard.R
+++ code/inference_hadamard.R
@@ -5,7 +5,7 @@ library(phangorn)
 ##################################################
 ###inference from SRHT sketching estimators
 
-# parameters used later：
+# parameters used later:
 # n: sample size
 # p: dimension 
 # m: sketch size 
@@ -42,7 +42,7 @@ Esticoef_SRHT<-function(m,c,X,y,partial=0){
 }
 
 ###################################################
-##Pivotal method 
+### Pivotal method 
 pivo_hadamard<-function(c,n,sX,sy,partial=0,alpha){
   m<-nrow(sX);p<-ncol(sX)
   xi<-p/n;gamma<-m/n
@@ -64,22 +64,20 @@ pivo_hadamard<-function(c,n,sX,sy,partial=0,alpha){
 }
 
 ###############################################################
-###Subsketching
-###Input: b is subskeching size, m is sketching size, X and y are used to generate repeated samples of smaller size b
-sub_int_hadamard<-function(b,m,c,X,y,sX,sy,K,alpha=0.05){
+### Sub_randomization
+### Input: b is the sketching size of new observations by repeating the sketching algorithms, m is the sketching size of the preliminary estimator, X and y are used to generate repeated samples of smaller size b
+sub_int_hadamard<-function(b,m,c,X,y,lssk,K,alpha=0.05){
   p<-ncol(X)
   pad<-padding(X,y)
   X1<-pad$padX;y1<-pad$pady
   n1<-nrow(X1)
   subske=0
   for(i in 1:K){
-    SXy<-Esticoef_SRHT(b,c,X,y,partial=0)$r3
-    g<-qr.solve(SXy[,1:p],SXy[,p+1])
-    subske[i]<-g[1]
+    subske[i]<-Esticoef_SRHT(b,c,X,y,partial=0)$r2
   }
   taum<-sqrt((m-p)*(n1-p)/(n1-m))
   taub<-sqrt((b-p)*(n1-p)/(n1-b))
-  lssk<-solve(t(sX)%*%sX)%*%(t(sX)%*%sy)
+  #lssk<-solve(t(sX)%*%sX)%*%(t(sX)%*%sy)
   center<-sum(c*lssk)
   lb<-quantile(taub*(subske-center),alpha/2)/(taum-taub)
   rb<-quantile(taub*(subske-center),(1-alpha/2))/(taum-taub)
@@ -88,7 +86,7 @@ sub_int_hadamard<-function(b,m,c,X,y,sX,sy,K,alpha=0.05){
 
 
 ##########################################
-###bootstrap
+### Bootstrap
 boot_int<-function(c,sX,sy,K,alpha){
   m<-nrow(sX);p<-ncol(sX)
   r_bootstrap<-matrix(0,p,K)
@@ -106,38 +104,38 @@ boot_int<-function(c,sX,sy,K,alpha){
 }
 
 ##################################
-###plug-in method
-plug_int<-function(c,m,X,y,sX,sy,K,alpha){
+### Multi-run plug-in 
+plug_int<-function(b,m,c,X,y,lssk,K,alpha){
   p<-ncol(X);n<-nrow(X)
+  Xy<-padding(X,y)
+  X1<-Xy$padX
+  n1=nrow(X1);
+  taum<-sqrt((m-p)*(n1-p)/(n1-m))
+  taub<-sqrt((b-p)*(n1-p)/(n1-b))
   plugske=0
   for(i in 1:K){
-    SXy<-Esticoef_SRHT(m,c,X,y,partial=0)$r3
-    g<-qr.solve(SXy[,1:p],SXy[,p+1])
-    plugske[i]<-g[1]
+    plugske[i]<-Esticoef_SRHT(b,c,X,y,partial=0)$r2
   }
   v<-var(plugske)
-  rb=qnorm(1-alpha/2,sd=sqrt(v))
-  lb=qnorm(alpha/2,sd=sqrt(v))
-  lssk<-solve(t(sX)%*%sX)%*%(t(sX)%*%sy)
+  rb=qnorm(1-alpha/2,sd=sqrt(v)*taub/taum)
+  lb=qnorm(alpha/2,sd=sqrt(v)*taub/taum)
+  #lssk<-solve(t(sX)%*%sX)%*%(t(sX)%*%sy)
   center<-sum(c*lssk)
   return(list(conf=c(center-rb,center-lb)))
 }
 
 ######################################
-### multi-run
-multi_run<-function(c,m,X,y,sX,sy,K,alpha){
+### Multi-run aggregation
+multi_run<-function(c,m,X,y,K,alpha){
   p<-ncol(X);n<-nrow(X)
-  plugske=0
+  multirun=0
   for(i in 1:K){
-    SXy<-Esticoef_SRHT(m,c,X,y,partial=0)$r3
-    g<-qr.solve(SXy[,1:p],SXy[,p+1])
-    plugske[i]<-g[1]
+    multirun[i]<-Esticoef_SRHT(m,c,X,y,partial=0)$r2
   }
-  v<-var(plugske)
+  v<-var(multirun)
   rb=qnorm(1-alpha/2,sd=sqrt(v/K))
   lb=qnorm(alpha/2,sd=sqrt(v/K))
-  
-  center<-mean(plugske)
+  center<-mean(multirun)
   return(list(conf=c(center-rb,center-lb)))
 }
 
@@ -145,10 +143,10 @@ multi_run<-function(c,m,X,y,sX,sy,K,alpha){
 
 ###############################################################
 ### compare the performence of the above five methods
-### input: grid_m: sequence of sketching size m; sim: number of Monte-Carlo simulations; alpha：coverage level
+### input: grid_m: sequence of sketching size of the preliminary estimator; grid_b:sequence of sketching size of new estimators; sim: number of Monte-Carlo simulations; alpha:coverage level
 ### output: the confidence intervals provided by the above five methods and the running time (measured in seconds)
 
-compare_methods_hadamard<-function(c,X,y,grid_m,b,K,sim,alpha=0.1){
+compare_methods_hadamard<-function(c,X,y,grid_m,grid_b,K,sim,alpha=0.1){
   p<-ncol(X);n<-nrow(X); Xty<-t(X)%*%y
   pivo_conf=sub_conf=boot_conf=plug_conf=multi_run_conf=matrix(0,sim,2*length(grid_m))
   pivo_time=sub_time=boot_time=plug_time=multi_run_time=matrix(0,sim,length(grid_m))
@@ -156,18 +154,19 @@ compare_methods_hadamard<-function(c,X,y,grid_m,b,K,sim,alpha=0.1){
     for(j in 1:length(grid_m)){
       SD<-Esticoef_SRHT(grid_m[j],c,X,y,partial=0)$r3
       SX<-SD[,1:p];Sy<-SD[,p+1]
+      lssk<-qr.solve(SX,Sy)
       
       Xy<-padding(X,y)
       X1<-Xy$padX;y1<-Xy$pady
       n1=nrow(X1);
-     
+      
       st1<-Sys.time()
       pivo_conf[i,(2*j-1):(2*j)]<-pivo_hadamard(c,n1,SX,Sy,partial=0,alpha)$conf
       ed1<-Sys.time()
       pivo_time[i,j] <- difftime(ed1, st1, units = "secs")
       
       st2<-Sys.time()
-      sub_conf[i,(2*j-1):(2*j)]<-sub_int_hadamard(b,grid_m[j],c,X,y,SX,Sy,K,alpha)$conf
+      sub_conf[i,(2*j-1):(2*j)]<-sub_int_hadamard(grid_b[j],grid_m[j],c,X,y,lssk,K,alpha)$conf
       ed2<-Sys.time()
       sub_time[i,j] <- difftime(ed2, st2, units = "secs")
       
@@ -177,12 +176,12 @@ compare_methods_hadamard<-function(c,X,y,grid_m,b,K,sim,alpha=0.1){
       boot_time[i,j]<-difftime(ed3, st3, units = "secs") 
       
       st4<-Sys.time()
-      plug_conf[i,(2*j-1):(2*j)]<-plug_int(c,grid_m[j],X,y,SX,Sy,K,alpha)$conf
+      plug_conf[i,(2*j-1):(2*j)]<-plug_int(grid_b[j],grid_m[j],c,X,y,lssk,K,alpha)$conf
       ed4<-Sys.time()
       plug_time[i,j]<-difftime(ed4, st4, units = "secs")
       
       st5<-Sys.time()
-      multi_run_conf[i,(2*j-1):(2*j)]<-multi_run(c,grid_m[j],X,y,SX,Sy,K,alpha)$conf
+      multi_run_conf[i,(2*j-1):(2*j)]<-multi_run(c,grid_b[j],X,y,K,alpha)$conf
       ed5<-Sys.time()
       multi_run_time[i,j]<-difftime(ed5, st5, units = "secs")
     }
