1f3e8d1c0b6572e76d84024a9c75835d75a3bb7f
Zhixiang Zhang <zzxiang0710@gmail.com>
2025-01-07 13:40:08 +0800

    Update usa1940_preprocessing.R

--- Experiment/sketched_least_squares/usa1940data/usa1940_preprocessing.R
+++ Experiment/sketched_least_squares/usa1940data/usa1940_preprocessing.R
@@ -2,21 +2,22 @@ library(readstata13)
 library(dplyr)
 library(caret)
 
-usa1940 <- readstata13::read.dta13("usa1940.dta")
-summary(usa1940)
-df0<-filter(usa1940,ind=='0124') #Industry code Educational service "https://usa.ipums.org/usa/volii/ind1940.shtml"
-df1<-subset(df0,select=-c(bpl,occ,ind))
+usa1940 <- readstata13::read.dta13("D:/Dropbox/R/sketching/code_randomized_algorithms/large real data/usa1940.dta")
+df0<-filter(usa1940,ind=='0124') 
+#Industry code Educational service "https://usa.ipums.org/usa/volii/ind1940.shtml"
 
-#perform one-hot encoding on dataframe
-dummy <- dummyVars(" ~ .", data=df1,fullRank = TRUE)
-df3 <- data.frame(predict(dummy, newdata=df1))
-df<-df3[,-(which(colSums(df3)==0))]
+df1<-droplevels(df0)
+df2<-subset(df1,select=-c(ind,ln_hr_wage))
 
-save(df,file="usa1940edu.rda")
+dummy <- dummyVars(" ~ .", data=df2,fullRank = TRUE)
 
+df3 <- data.frame(predict(dummy, newdata=df2))
+df4<-df3[,-(which(colSums(df3)==1))]
 
-######################################################################
-load("D:/Dropbox/R/sketching/code_randomized_algorithms/large real data/usa1940edu.rda")
+occ_count <- sum(grepl("occ", names(df4)))
+n<-nrow(df4);p<-ncol(df4)
+c<-rep(0,p)
+c[p-occ_count]<-1
 
-y<-as.numeric(df$ln_hr_wage)
-X<-as.matrix(subset(df,select=-c(ln_hr_wage)))
+y<-as.vector(df0$ln_hr_wage)
+X<-as.matrix(df4)
